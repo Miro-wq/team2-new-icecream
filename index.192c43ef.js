f604552188fb0dbde9a88b044d80647f23b3c3a8
Miro-wq <117939922+Miro-wq@users.noreply.github.com>
2024-04-15 19:22:12 +0000

    Deploying to gh-pages from @ Miro-wq/team2-new-icecream@1e6154ca66f1ac61ef86f1e20b8f5202353798a6 🚀

---
+++ index.192c43ef.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".howitsmade-btn").click((function(){$(".modal-overlay").fadeIn()})),$(".close-modal").click((function(){$(".modal-overlay").fadeOut()}))}));
+//# sourceMappingURL=index.192c43ef.js.map
