1e6154ca66f1ac61ef86f1e20b8f5202353798a6
Miro <117939922+Miro-wq@users.noreply.github.com>
2024-04-15 22:17:28 +0300

    update sections and modals
    Update index.html, java, hero-header.html, sec2.html, header-hero.scss, sec2.scss

---
+++ src/js/cow-modal.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('.howitsmade-btn').click(function () {
+    $('.modal-overlay').fadeIn();
+  });
+
+  $('.close-modal').click(function () {
+    $('.modal-overlay').fadeOut();
+  });
+});
