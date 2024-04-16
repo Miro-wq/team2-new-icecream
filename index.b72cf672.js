b614d784e3f5152e941729023b4d411f72a23912
Miro-wq <117939922+Miro-wq@users.noreply.github.com>
2024-04-16 20:34:57 +0000

    Deploying to gh-pages from @ Miro-wq/team2-new-icecream@d2e46898d9adb9380d00dc9d4f4d0d62638ca5a6 🚀

---
+++ index.b72cf672.js
@@ -0,0 +1,2 @@
+var popup=document.getElementById("popup");function openPopup(){popup.classList.add("open-popup")}function closePopup(){popup.classList.remove("open-popup")}
+//# sourceMappingURL=index.b72cf672.js.map
